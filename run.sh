521c02daa10202cd98e77a0fc34e3eaa9a892fd8
vaakinmade <vickeyakinmade22@gmail.com>
2017-03-21 22:38:08 +0000

    Script to Start virtualenv. Install requirements.txt. Run test script.

--- run.sh
+++ run.sh
@@ -4,18 +4,9 @@ if [ ! -d "venv" ]; then
         virtualenv venv
 fi
 . venv/bin/activate
-# Create a virtualenv
-# virtualenv --python=python3.4 LagoApp
-# Activate the virtualenv
-# source LagoApp/bin/activate
+
 # Install project Python packages
-pip install -r requirements.txt
-# Setup required environment variables
-# export DJANGO_SETTINGS_MODULE=config.settings.local
-# export DATABASE_URL=postgres://jenkins:jenkins@localhost/jenkins
-# Apply migrations
-./manage.py migrate
+pip install -r requirements.txt  --download-cache=/tmp/$JOB_NAME
+
 # Run the django-jenkins management command
-./manage.py jenkins --enable-coverage --coverage-html-report=htmlcov --coverage-rcfile .coveragerc --max-complexity 10
-# Delete the virtualenv
-#rm -rf LagoApp
+python manage.py jenkins --enable-coverage

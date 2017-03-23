ba44226a5ee802492afe34975a20a8eb8162d0c5
vaakinmade <vickeyakinmade22@gmail.com>
2017-03-23 15:21:08 +0100

    Add whitenoise to project

--- lagopoly/deploy_settings/__init__.py
+++ lagopoly/deploy_settings/__init__.py
@@ -13,3 +13,5 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"

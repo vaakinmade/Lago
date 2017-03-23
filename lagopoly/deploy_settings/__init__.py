ae0f8e2d3545927321a3a77f9e5c1c8d84e25012
vaakinmade <vickeyakinmade22@gmail.com>
2017-03-23 14:48:05 +0100

    Added db settings for postgres migration to heroku

--- lagopoly/deploy_settings/__init__.py
+++ lagopoly/deploy_settings/__init__.py
@@ -1,3 +1,4 @@
+import dj_database_url
 from lagopoly.settings import *
 
 DEBUG = False
@@ -9,3 +10,6 @@ ALLOWED_HOSTS = [
 ]
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
+
+db_from_env = dj.dj_database_url.config()
+DATABASES['default'].update(db_from_env)

3ff274ed467ce1177ee6c568f160e115c8d2e7d9
vaakinmade <vickeyakinmade22@gmail.com>
2017-03-23 16:48:52 +0100

    changed STATIC_ROOT

--- lagopoly/settings.py
+++ lagopoly/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 LOGIN_REDIRECT_URL = "listings:list"

458af70d2deddc5fe49a7c6582100a79333a1ca0
vaakinmade <vickeyakinmade22@gmail.com>
2017-06-26 14:02:56 +0100

    set to debug back to false

--- lagopoly/settings.py
+++ lagopoly/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = config('SECRET_KEY')
 GOOGLE_RECAPTCHA_SECRET_KEY = config('GOOGLE_RECAPTCHA_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
